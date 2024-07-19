e2acb32f1ac2502d22beff6c147ee96ec591d439
Brad Parham <brad.a.parham@intel.com>
2024-07-19 11:31:12 +0200

    Add support to publish new versions automatically

--- tools/release.sh
+++ tools/release.sh
@@ -13,11 +13,20 @@ git config user.name Brad Parham
 git add index.json
 
 # Bump patch version with new changes
-yarn version --patch --message "bump version to %s"
+yarn version patch
+
+git add package.json
+
+VERSION=$(node -p -e "require('./package.json').version")
+
+# Commit and tag the repo
+git commit -m "bump version to v${VERSION}"
+git tag v${VERSION}
 
 # Publish new version to NPM
 echo //registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN} > .npmrc
 npm publish --registry=https://registry.npmjs.org/
 rm -rf .npmrc
 
+# Push commit and tag to remote
 git push origin main --follow-tags
